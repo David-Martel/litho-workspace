e0a6fedcea734c51b7f7f0e90fcd24cc5bf69eef
David T. Martel <damartel@umich.edu>
2026-02-22 18:55:40 -0500

    chore: remove temp build script

--- build_test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 >nul 2>&1
-set RUSTC_WRAPPER=
-set CARGO_TARGET_DIR=T:\RustCache\cargo-target
-cd /d C:\codedev\litho-workspace
-T:\RustCache\rustup\toolchains\stable-x86_64-pc-windows-msvc\bin\cargo.exe test -p litho-extract
